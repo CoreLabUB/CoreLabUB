633e1dae6364216b7e9d7624361c75406bea81ed
Miquel <miquel.forcada@enti.cat>
2025-07-04 12:18:35 +0200

    Changed Language Localization

--- Assets/Scripts/Miquel/Popups/PopupsManager.cs
+++ Assets/Scripts/Miquel/Popups/PopupsManager.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Assets.SimpleLocalization.Scripts;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
+
+public enum Language { Catalan, Spanish, English }
+
 public class PopupsManager : Singleton<PopupsManager>
 {
+    [SerializeField] Language activeLanguage;
+
     private int currentPopupId = 0;
     private TMP_Text popupText;
 
-    Dictionary<int, string> popups = new()
-    {
-        {0, "GRAB A CARD\nAND GO TO ARCHEOLAB"},
-        {1, "OVER HERE"},
-    };
+    Dictionary<int, string> localizationKeys = new();
 
     void Start()
     {
+        LocalizationManager.Read();
+        LocalizationManager.Language = activeLanguage.ToString();
+
+        var temp = LocalizationManager.Dictionary;
+        int count = 0;
+        foreach ( var key in temp.Values )
+        {
+            foreach (var keys in key.Keys)
+            {
+                localizationKeys.Add(count, keys);
+                count++;
+            }
+        }
+
         popupText = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TMP_Text>();
     }
 
@@ -27,10 +44,8 @@ public class PopupsManager : Singleton<PopupsManager>
         if (currentPopupId != id) { return; }
        
         popupText.transform.parent.gameObject.SetActive(true);
-        popupText.text = popups[currentPopupId];
+        popupText.text = LocalizationManager.Localize(localizationKeys[id]);
         popupText.transform.parent.parent.SetPositionAndRotation(pos, rot);
-
-        
     }
 
     public void ClosePopup(int id)
